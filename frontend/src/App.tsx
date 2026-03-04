da6a902a1d9fb1dffafc22e8cd6635a53237c74b
Ali Sami <theforgettenone2@gmail.com>
2026-03-04 21:42:00 +0545

    nothing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <CustomCursor />
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/alixsami.github.io">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
